e7a5d8ec278ca47206586f370df5e761ffedbb95
Sofia Abigail Sabán Cárcamo <201800090@colsanjose.edu.gt>
2020-07-09 14:47:55 -0600

    1ra. Parte

--- Proyecto_Arduino1/Proyecto_Arduino1/Program.cs
+++ Proyecto_Arduino1/Proyecto_Arduino1/Program.cs
@@ -4,45 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO.Ports;
-using System.Security.Cryptography.X509Certificates;
-using System.Runtime.CompilerServices;
+
 
 namespace Proyecto_Arduino1
 {
-    public class Program
+    class Program
     {
-        static char[,] datos = new Char[3, 3];
-        static SerialPort Arduino = new SerialPort("COMS", 9600);
+        
+        static SerialPort Arduino = new SerialPort("COM27", 9600);
 
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
             
+            Arduino = Open();
+
             Arduino.Parity = Parity.None;
             Arduino.StopBits = StopBits.One;
             Arduino.DataBits = 8;
             Arduino.Handshake = Handshake.None;
             Arduino.RtsEnable = true;
-          
+
+            // Mi grupo: Keyli Solis, Fatima Ventura, y mi persona: (Sofia Sabán)
+
             if (Arduino.IsOpen)
             {
                 Console.WriteLine("Com abierto");
                 byte[] data = Encoding.ASCII.GetBytes("z");
                 Arduino.Write(data, 0, data.Length);
 
-                Arduino.Write("i");
-            }
-            else
-            {
-                Console.WriteLine("Com cerrado");
-            }
-
-
-            if (Arduino.IsOpen)
-            {
                 Console.WriteLine("Com abierto");
-                byte[] data = Encoding.ASCII.GetBytes("p");
-                Arduino.Write(data, 0, data.Length);
+                byte[] data1 = Encoding.ASCII.GetBytes("p");
+                Arduino.Write(data1, 0, data1.Length);
 
+                Console.WriteLine("Com abierto");
+                byte[] data2 = Encoding.ASCII.GetBytes("k");
+                Arduino.Write(data2, 0, data2.Length);
                 Arduino.Write("l");
             }
             else
@@ -50,18 +46,8 @@ namespace Proyecto_Arduino1
                 Console.WriteLine("Com cerrado");
             }
 
-            if (Arduino.IsOpen)
-            {
-                Console.WriteLine("Com abierto");
-                byte[] data = Encoding.ASCII.GetBytes("k");
-                Arduino.Write(data, 0, data.Length);
-
-                Arduino.Write("f");
-            }
-            else
-            {
-                Console.WriteLine("Com cerrado");
-            }
+            Console.ReadKey();
+            Arduino.Close();
         }
     }
 }
