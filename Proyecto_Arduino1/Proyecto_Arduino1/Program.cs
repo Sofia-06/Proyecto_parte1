8e079be9ffecd4ac9fe612fefb4e09992860b473
Sofia Abigail Sabán Cárcamo <201800090@colsanjose.edu.gt>
2020-07-09 10:41:01 -0600

    Parte1

---
+++ Proyecto_Arduino1/Proyecto_Arduino1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Security.Cryptography.X509Certificates;
+using System.Runtime.CompilerServices;
+
+namespace Proyecto_Arduino1
+{
+    public class Program
+    {
+        static char[,] datos = new Char[3, 3];
+        static SerialPort Arduino = new SerialPort("COMS", 9600);
+
+        public static void Main(string[] args)
+        {
+            
+            Arduino.Parity = Parity.None;
+            Arduino.StopBits = StopBits.One;
+            Arduino.DataBits = 8;
+            Arduino.Handshake = Handshake.None;
+            Arduino.RtsEnable = true;
+          
+            if (Arduino.IsOpen)
+            {
+                Console.WriteLine("Com abierto");
+                byte[] data = Encoding.ASCII.GetBytes("z");
+                Arduino.Write(data, 0, data.Length);
+
+                Arduino.Write("i");
+            }
+            else
+            {
+                Console.WriteLine("Com cerrado");
+            }
+
+
+            if (Arduino.IsOpen)
+            {
+                Console.WriteLine("Com abierto");
+                byte[] data = Encoding.ASCII.GetBytes("p");
+                Arduino.Write(data, 0, data.Length);
+
+                Arduino.Write("l");
+            }
+            else
+            {
+                Console.WriteLine("Com cerrado");
+            }
+
+            if (Arduino.IsOpen)
+            {
+                Console.WriteLine("Com abierto");
+                byte[] data = Encoding.ASCII.GetBytes("k");
+                Arduino.Write(data, 0, data.Length);
+
+                Arduino.Write("f");
+            }
+            else
+            {
+                Console.WriteLine("Com cerrado");
+            }
+        }
+    }
+}
